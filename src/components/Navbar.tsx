6195e168ad94c6222d9edbe6c5c8756d8eb030cd
prateekkumar77 <prateekkumar77@gmail.com>
2023-05-16 22:13:05 +0530

    Dark Mode Button Added to Nav Bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { Center, HStack, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import logo from '../assets/keen.png'
+import DarkModeSwitch from './DarkModeSwitch'
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack spacing={15} alignItems='center'>
         <Image src={logo} boxSize='60px'/>
-        <Text>Keen Clone</Text>
+        <Text >Keen Clone</Text>
+        <DarkModeSwitch />
     </HStack>
   )
 }
